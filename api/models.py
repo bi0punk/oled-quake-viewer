d1fd09e0ad0eab24dc70467a811fa6909a89ff3c
bi0punk <systecinfo_cl@criptext.com>
2024-02-19 23:53:49 -0300

    add functions

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Quake(models.Model):
+    fecha_local = models.DateTimeField()
+    fecha_utc = models.DateTimeField()
+    lugar = models.CharField(max_length=255)
+    latitud = models.DecimalField(max_digits=9, decimal_places=6)
+    longitud = models.DecimalField(max_digits=9, decimal_places=6)
+    profundidad = models.DecimalField(max_digits=8, decimal_places=2)
+    magnitud = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.lugar} - {self.fecha_local.strftime('%Y-%m-%d %H:%M:%S')}"
--- quakeApi/urls.py
+++ quakeApi/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Importa include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Incluye las URLs de la aplicación api
+    path('api/', include('api.urls')),  # Asegúrate de que el nombre de la aplicación sea correcto
 ]
